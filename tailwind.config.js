316441f9df93a5303ed2d17f2144fdd9f51d9524
jasesio <jasesio@uoc.edu>
2024-06-09 11:56:02 +0200

    my first commit

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,scss}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+        ],
+      },
+      colors: {
+        'primary-color': '#1e1e1e',
+        'secondary-color': '#f3ca52',
+        'tertiary-color': '#dc3545',
+
+      },
+    },
+  },
+  plugins: [],
+};
+
