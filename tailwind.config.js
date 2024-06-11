a85a7f64d1123e1ae67cc2fc31bff7e30d0a08b8
jasesio <jasesio@uoc.edu>
2024-06-11 07:23:39 +0200

    mejoras

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,10 +21,16 @@ module.exports = {
         'primary-color': '#1e1e1e',
         'secondary-color': '#f3ca52',
         'tertiary-color': '#dc3545',
-
       },
+      maxHeight: {
+        '350': '350px',
+      },
+      backgroundPosition: {
+        'center-top': 'center top',
+        'center-bottom': 'center bottom',
+      },
+  
     },
   },
   plugins: [],
 };
-
